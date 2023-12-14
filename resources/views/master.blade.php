6254d4a3bfd83b2593a18f7be83a36358eb353b8
Clint Responso <clint.responso@copypaste.ph>
2023-12-14 18:18:09 +0800

    fixing name

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -8,13 +8,13 @@
         <link rel="preconnect" href="https://fonts.googleapis.com"> 
         <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin> 
         
-        <title>Darrel B Dyal Author Of The Oracles of God | Darrel B Dyal Author</title>
-        <meta name="description" content="Darrel B Dyal is the author of Book The Oracles of God, Darrel B Dyal also wrote a book called Limpy, For such a time as this and more." />
+        <title>Darell B. Dyal Author Of The Oracles of God | Darell B. Dyal Author</title>
+        <meta name="description" content="Darell B. Dyal is the author of Book The Oracles of God, Darell B. Dyal also wrote a book called Limpy, For such a time as this and more." />
         <link rel="canonical" href="https://www.booksbyauthordarellbdyal.com" />
         <meta property="og:locale" content="en_US" />
         <meta property="og:type" content="website" />
-        <meta property="og:title" content="Darrel B Dyal Author Of The Oracles of God | Darrel B Dyal Author" />
-        <meta property="og:description" content="Darrel B Dyal is the author of Book The Oracles of God, Darrel B Dyal also wrote a book called Limpy, For such a time as this and more." />
+        <meta property="og:title" content="Darell B. Dyal Author Of The Oracles of God | Darell B. Dyal Author" />
+        <meta property="og:description" content="Darell B. Dyal is the author of Book The Oracles of God, Darell B. Dyal also wrote a book called Limpy, For such a time as this and more." />
         <meta property="og:url" content="https:///www.booksbyauthordarellbdyal.com" />
         <meta property="og:site_name" content="Darell B. Dyal" />
         <meta property="og:image" content="http:///www.booksbyauthordarellbdyal.com/storage/01HGT4RFDVX17SY2ZSFVBJP68Z.jpg" />
